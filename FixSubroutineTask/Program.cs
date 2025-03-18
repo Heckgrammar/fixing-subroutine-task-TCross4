d54265e93076f961f8625c7f29ac039424a7c8a9
TCross4 <144451394+TCross4@users.noreply.github.com>
2025-03-18 18:08:47 +0000

    Update Program.cs

--- FixSubroutineTask/Program.cs
+++ FixSubroutineTask/Program.cs
@@ -4,15 +4,15 @@
     {
         static void Main(string[] args)
         {
-            for(int i = 8;i >= 0; i++) {
+            for(int i = 0;i < 8; i++) //Used to be for(int i = 8;i >= 0; i++)
+            {
                 Console.WriteLine(diffCurrencies(i));            
             }
         }
         static string diffCurrencies(int x)
         {
             string[] currencies = { "baht", "dollar", "euro", "koruna", "lira", "rand", "rupee", "yen" };
-            return currencies[1];
-
+            return currencies[x]; //Used to be return currencies[1];
         }
     }
 }
